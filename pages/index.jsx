c5a6cfd962364d3ef1c7112e816b3b807076194c
MrG788 <143547454+MrG788@users.noreply.github.com>
2025-12-02 23:38:07 +0000

    Create index.jsx

---
+++ pages/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Coffee Demo Website Ready</h1>
+      <p>Your full site layout will appear here.</p>
+    </main>
+  );
+}
